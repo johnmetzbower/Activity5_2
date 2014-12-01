0593a5386417935aaff963b66022ddc8d8e265be
johnmetzbower <johnmetzbower@nokware.net>
2014-11-30 18:38:35 -0600

    what are the errors and why?

---
+++ Activity5_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity5_2
+{
+    class Program
+        {
+            static List<int> numList = new List<int>();
+            static void Main(string[] args)
+            {
+                LoadList(10);
+                foreach (int i in numList)
+                {
+                System.Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
+            static void LoadList(int imax);
+            {       
+                for (int i = 1; i <= 10; 1++)
+                {
+                    numList.Add(i);
+                }
+            }
+        }
